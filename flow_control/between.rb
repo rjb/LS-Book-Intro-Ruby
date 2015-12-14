66c6d05f8e68cae831f1147dc8e0898c62d4c6ee
Raffy Banks <raffybanks@gmail.com>
2015-12-13 22:24:45 -0800

    Flow Control chapter exercise 5 solution

--- flow_control/between.rb
+++ flow_control/between.rb
@@ -1,12 +1,31 @@
+def if_range(number)
+  if number < 0
+    puts "Oops! Number needs to be between 0 and 100"
+  elsif number <= 50
+    puts "#{number} is between 0 and 50"
+  elsif number <= 100
+    puts "#{number} is between 51 and 100"
+  else
+    puts "#{number} is greater than 100"
+  end
+end
+
+
+def case_range(number)
+  case
+  when number < 0
+    puts "Oops! Number needs to be between 0 and 100"
+  when number <= 50
+    puts "#{number} is between 0 and 50"
+  when number <= 100
+    puts "#{number} is between 51 and 100"
+  else
+    puts "#{number} is greater than 100"
+  end
+end
+
 puts "Enter a number between 0 and 100:"
 number = gets.chomp.to_i
 
-if number < 0
-  puts "Oops! Number needs to be between 0 and 100"
-elsif number <= 50
-  puts "#{number} is between 0 and 50"
-elsif number <= 100
-  puts "#{number} is between 51 and 100"
-else
-  puts "#{number} is greater than 100"
-end
+if_range(number)
+case_range(number)
